18580b6c64785f5f2e5bbd42be5a589c8da8a921
John Lago <750845+Lagoja@users.noreply.github.com>
2018-04-06 23:15:15 -0700

    Initial Commit

---
+++ TouchbarWeather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TouchbarWeather
+//
+//  Created by John Lago on 4/6/18.
+//  Copyright © 2018 John Lago. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
